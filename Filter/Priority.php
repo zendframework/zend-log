761eac13082c7984394aa88b942417727bf29a33
Marco Pivetta <ocramius@gmail.com>
2014-06-23 19:54:06 +0000

    Importing state as of zendframework/zf2@2a9c97db0b21336d85937caaec2ecda3de6dc474 (1403553246)
    Automatic import via rsync

--- Filter/Priority.php
+++ Filter/Priority.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -42,14 +42,14 @@ class Priority implements FilterInterface
             $operator = isset($priority['operator']) ? $priority['operator'] : null;
             $priority = isset($priority['priority']) ? $priority['priority'] : null;
         }
-        if (!is_int($priority) && !ctype_digit($priority)) {
+        if (!is_int($priority)) {
             throw new Exception\InvalidArgumentException(sprintf(
-                'Priority must be a number, received "%s"',
+                'Priority must be an integer; received "%s"',
                 gettype($priority)
             ));
         }
 
-        $this->priority = (int) $priority;
+        $this->priority = $priority;
         $this->operator = $operator === null ? '<=' : $operator;
     }
 
--- Processor/RequestId.php
+++ Processor/RequestId.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
 *
 * @link      http://github.com/zendframework/zf2 for the canonical source repository
-* @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+* @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
 * @license   http://framework.zend.com/license/new-bsd New BSD License
 */
 
@@ -21,7 +21,7 @@ class RequestId implements ProcessorInterface
     protected $identifier;
 
     /**
-     * Adds an identifier for the request to the log.
+     * Adds a identifier for the request to the log.
      *
      * This enables to filter the log for messages belonging to a specific request
      *
@@ -49,7 +49,7 @@ class RequestId implements ProcessorInterface
             return $this->identifier;
         }
 
-        $requestTime = (PHP_VERSION_ID >= 50400)
+        $requestTime = (version_compare(PHP_VERSION, '5.4.0') >= 0)
                      ? $_SERVER['REQUEST_TIME_FLOAT']
                      : $_SERVER['REQUEST_TIME'];
 

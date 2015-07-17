61fc0bfa36e222cb2f43a98c82ff4da9bd9c827d
Aleksey Khudyakov <aleksey@xerkus.pro>
2015-07-18 04:20:16 +1000

    Change logger adapter test to extend PSR-3 compliance test

--- test/Psr3LoggerAdapterTest.php
+++ test/Psr3LoggerAdapterTest.php
@@ -9,18 +9,72 @@
 
 namespace ZendTest\Log;
 
-use PHPUnit_Framework_TestCase as TestCase;
 use Psr\Log\LogLevel;
+use Psr\Log\Test\LoggerInterfaceTest;
 use Zend\Log\Logger;
 use Zend\Log\Psr3LoggerAdapter;
+use Zend\Log\Writer\Mock as MockWriter;
 
 /**
  * @group Zend_Log
  * @coversDefaultClass Zend\Log\Psr3LoggerAdapter
  * @covers ::<!public>
  */
-class Psr3LoggerAdapterTest extends TestCase
+class Psr3LoggerAdapterTest extends LoggerInterfaceTest
 {
+    /**
+     * @var array
+     */
+    protected $psr3PriorityMap = [
+        LogLevel::EMERGENCY => Logger::EMERG,
+        LogLevel::ALERT     => Logger::ALERT,
+        LogLevel::CRITICAL  => Logger::CRIT,
+        LogLevel::ERROR     => Logger::ERR,
+        LogLevel::WARNING   => Logger::WARN,
+        LogLevel::NOTICE    => Logger::NOTICE,
+        LogLevel::INFO      => Logger::INFO,
+        LogLevel::DEBUG     => Logger::DEBUG,
+    ];
+
+    /**
+     * Provides logger for LoggerInterface compat tests
+     *
+     * @return Psr3LoggerAdapter
+     */
+    public function getLogger()
+    {
+        $this->mockWriter = new MockWriter;
+        $logger = new Logger;
+        $logger->addProcessor('psr3placeholder');
+        $logger->addWriter($this->mockWriter);
+        return new Psr3LoggerAdapter($logger);
+    }
+
+    /**
+     * This must return the log messages in order.
+     *
+     * The simple formatting of the messages is: "<LOG LEVEL> <MESSAGE>".
+     *
+     * Example ->error('Foo') would yield "error Foo".
+     *
+     * @return string[]
+     */
+    public function getLogs()
+    {
+        $prefixMap = array_flip($this->psr3PriorityMap);
+        $convert = function ($event) use ($prefixMap) {
+            $prefix = $prefixMap[$event['priority']];
+            $message = $prefix . ' ' . $event['message'];
+            return $message;
+        };
+        return array_map($convert, $this->mockWriter->events);
+    }
+
+    public function tearDown()
+    {
+        unset($this->mockWriter);
+    }
+
     /**
      *
      * @covers ::__construct
@@ -37,65 +91,15 @@ class Psr3LoggerAdapterTest extends TestCase
     }
 
     /**
-     *
-     * @covers ::emergency
-     * @covers ::alert
-     * @covers ::critical
-     * @covers ::error
-     * @covers ::warning
-     * @covers ::notice
-     * @covers ::info
-     * @covers ::debug
-     * @dataProvider levelSpecificMethodsToPriorityProvider
-     */
-    public function testPsr3LevelSpecificMethods($method, $priority)
-    {
-        $message = 'foo';
-        $context = ['bar' => 'baz'];
-
-        $logger = $this->getMock('Zend\Log\Logger', ['log']);
-        $logger->expects($this->once())
-            ->method('log')
-            ->with(
-                $this->equalTo($priority),
-                $this->equalTo($message),
-                $this->equalTo($context)
-            );
-
-        $adapter = new Psr3LoggerAdapter($logger);
-        $adapter->{$method}($message, $context);
-    }
-
-    /**
-     *
-     * Data provider
-     *
-     * @return array
-     */
-    public function levelSpecificMethodsToPriorityProvider()
-    {
-        return [
-            ['emergency', Logger::EMERG],
-            ['alert', Logger::ALERT],
-            ['critical', Logger::CRIT],
-            ['error', Logger::ERR],
-            ['warning', Logger::WARN],
-            ['notice', Logger::NOTICE],
-            ['info', Logger::INFO],
-            ['debug', Logger::DEBUG],
-        ];
-    }
-
-    /**
      * @covers ::log
      * @dataProvider logLevelsToPriorityProvider
      */
-    public function testPsr3LogLevels($logLevel, $priority)
+    public function testPsr3LogLevelsMapsToPriorities($logLevel, $priority)
     {
         $message = 'foo';
         $context = ['bar' => 'baz'];
 
-        $logger = $this->getMock('Zend\Log\Logger', ['log']);
+        $logger = $this->getMock(Logger::class, ['log']);
         $logger->expects($this->once())
             ->method('log')
             ->with(
@@ -115,29 +119,10 @@ class Psr3LoggerAdapterTest extends TestCase
      */
     public function logLevelsToPriorityProvider()
     {
-        return [
-            [LogLevel::EMERGENCY, Logger::EMERG],
-            [LogLevel::ALERT, Logger::ALERT],
-            [LogLevel::CRITICAL, Logger::CRIT],
-            [LogLevel::ERROR, Logger::ERR],
-            [LogLevel::WARNING, Logger::WARN],
-            [LogLevel::NOTICE, Logger::NOTICE],
-            [LogLevel::INFO, Logger::INFO],
-            [LogLevel::DEBUG, Logger::DEBUG],
-        ];
-    }
-
-    /**
-     *
-     * @covers ::log
-     */
-    public function testThrowsExceptionOnUnknownLevel()
-    {
-        $this->setExpectedException('Psr\Log\InvalidArgumentException');
-
-        $logger = $this->getMock('Zend\Log\Logger');
-
-        $adapter = new Psr3LoggerAdapter($logger);
-        $adapter->log('unknown', 'foo');
+        $return = [];
+        foreach ($this->psr3PriorityMap as $level => $priority) {
+            $return[] = [$level, $priority];
+        }
+        return $return;
     }
 }

432d8afc42267e125d5d766df996d93ad1d80548
Gianluca Arbezzano <gianarb92@gmail.com>
2015-09-03 01:57:19 +0200

    Clean interface

--- src/LoggerAwareInterface.php
+++ src/LoggerAwareInterface.php
@@ -21,12 +21,4 @@ interface LoggerAwareInterface
      * @return void
      */
     public function setLogger(LoggerInterface $logger);
-
-    /**
-     * Get logger instance. Currently commented out as this would possibly break
-     * existing implementations.
-     *
-     * @return null|LoggerInterface
-     */
-    // public function getLogger();
 }

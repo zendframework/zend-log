b6bc767c035eef0747beb50e85e61ee779703280
Gianluca Arbezzano <gianarb92@gmail.com>
2015-09-03 02:04:19 +0200

    return inconsistency between interface and its implementation

--- src/Formatter/FormatterInterface.php
+++ src/Formatter/FormatterInterface.php
@@ -22,7 +22,7 @@ interface FormatterInterface
      * Formats data into a single line to be written by the writer.
      *
      * @param array $event event data
-     * @return string formatted line to write to the log
+     * @return array formatted line to write to the log
      */
     public function format($event);
 

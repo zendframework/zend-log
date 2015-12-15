45e4cacacdae85ab30fe38d061696325cc8986ce
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2015-12-15 13:20:24 +0100

    Replaced the headers

--- src/Writer/FormatterPluginManager.php
+++ src/Writer/FormatterPluginManager.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * @author    Antoine Hedgecock <antoine.hedgecock@gmail.com>
+ * Zend Framework (http://framework.zend.com/)
  *
- * @copyright Interactive Solutions AB
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace Zend\Log\Writer;
